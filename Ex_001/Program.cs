95def6764fe71b497e038687cced37afbd07ba61
Ekaterina Anikina <reityan97@mail.ru>
2022-08-24 20:56:43 +0300

    Решение 1 задачи (9 семинар)

---
+++ Ex_001/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задайте значения M и N. Напишите программу, 
+//которая выведет все натуральные числа кратные 3-ём в промежутке от M до N.
+
+void Zadacha3()
+{
+    Console.WriteLine("Введите число m:");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число n:");
+    int n = Convert.ToInt32(Console.ReadLine());
+   
+    Recursion(m,n);
+   
+}
+
+
+void Recursion(int m, int n)
+{
+    if (m > n) 
+    {
+        return;
+    }
+    if (m%3 == 0) Console.Write(m + " ");
+    m++;
+    
+    Recursion(m,n);
+}
+
+Zadacha3();
